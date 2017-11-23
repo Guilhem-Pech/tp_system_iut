692fcc144a1713dfc0fbc9e6db999bf137ac8679
Daryl Winters <guilhempech@gmail.com>
2017-11-23 11:01:45 +0100

    Tp processus : exo_01a
    	nouveau fichier : dirproc/Makefile
    	nouveau fichier : dirproc/codeobjsyst
    	nouveau fichier : dirproc/exo_01.cxx
    	nouveau fichier : dirproc/nsSysteme.cxx
    	nouveau fichier : include/CExc.h
    	nouveau fichier : include/INCLUDE_H
    	nouveau fichier : include/nsSysteme.h

---
+++ tpsystem/tpprocessus/exo_01/dirproc/exo_01.cxx
@@ -0,0 +1,52 @@
+/**
+ *
+ * @File : 
+ *
+ * @Author : A. B. Dragut
+ *
+ * @Synopsis : modelmain
+ **/
+
+#include <string>
+#include <exception>
+#include <iostream>
+
+#include <unistd.h>     // getdtablesize()
+#include <sys/time.h>   // fd_set
+
+#include "nsSysteme.h"
+#include "CExc.h"
+
+using namespace nsSysteme;
+using namespace std;
+
+int main(int argc, char * argv [])
+{
+  try {
+	if(pid_t pidFils = fork()){ // On est donc dans le processus père
+		 cout << "PID du père : " << ::getpid() 
+		      << " PID du fils : " << pidFils << endl;
+	}
+	else { // On est donc dans le processus fils
+
+		cout << "PID (Fils): " << ::getpid() <<
+			" fils de :" << ::getppid() << endl;
+	} 
+    return 0;
+  }
+  catch (const CExc & Exc) {
+        cerr <<Exc<< endl;
+        return errno;
+  }
+  catch (const exception & Exc) {
+        cerr << "Exception : " << Exc.what () << endl;
+        return 1;
+  }
+  catch (...) {
+        cerr << "Exception inconnue recue dans la fonction main()" 
+             << endl;
+        return 1;
+  }
+
+
+}  //  main()
